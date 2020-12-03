b481bfe0fdd4af09ce848a26f2781560a77ce452
sakew <dorinsecu@gmail.com>
2020-12-03 19:01:16 +0200

    added a recursive method for binary search algorithm

--- searchAlgorithms/BinarySearch.java
+++ searchAlgorithms/BinarySearch.java
@@ -25,4 +25,25 @@ public class BinarySearch {
 		return -1;
 	}
 	
+	public int recursiveBinarySearch(int[] input, int value) {
+		return recursiveBinarySearch(input, 0, input.length, value);
+	}
+	public int recursiveBinarySearch(int[] input, int start, int end, int value) {
+		if (start >= end) {
+			return -1;
+		}
+		
+		int mid = (start + end) / 2;
+		System.out.println("mid = " + mid);
+		
+		if (input[mid] == value) {
+			return mid;
+		}
+		else if (input[mid] < value) {
+			return recursiveBinarySearch(input, mid + 1, end, value);
+		}
+		else {
+			return recursiveBinarySearch(input, start, mid, value);
+		}
+	}
 }
