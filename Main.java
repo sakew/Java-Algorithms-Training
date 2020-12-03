4805d0e4949e19a1b78eae1491811188bd7492ec
sakew <dorinsecu@gmail.com>
2020-12-03 17:45:26 +0200

    hashtable challenge

--- Main.java
+++ Main.java
@@ -1,18 +1,13 @@
-import sortingAlgorithms.BucketSort;
+import listsExercices.HashtableChallenge;
 
 public class Main {
 
 	public static void main(String[] args) {
 			
 		
-		int[] intArray = { 54, 46, 83, 66, 95, 92, 43 };
+		HashtableChallenge challenge = new HashtableChallenge();
 		
-		BucketSort bucket = new BucketSort();
-		bucket.bucketSort(intArray);
-		
-		for (int i = 0; i < intArray.length; i++) {
-			System.out.print(intArray[i] + ", ");
-		}
+		challenge.challenge();
 	}
 			
 }
