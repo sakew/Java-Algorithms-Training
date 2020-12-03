a3d391e3c32ce2adb62be59bf466a5b764bfe907
sakew <dorinsecu@gmail.com>
2020-12-04 01:50:29 +0200

    Binary Search Tree insertion and traversal methods (in-order traversal)

--- Main.java
+++ Main.java
@@ -1,16 +1,24 @@
-import searchAlgorithms.BinarySearch;
+import Trees.Tree;
 
 public class Main {
 
 	public static void main(String[] args) {
 			
-		int[] intArray = { -22, -15, 1, 7, 20, 35, 55 };
+		Tree intTree = new Tree();
 		
-		BinarySearch search = new BinarySearch();
+//		The values for the tree are: { 25, 20, 15, 27, 30, 29, 26, 22, 32 }
 		
-		System.out.println(search.iterativeBinarySearch(intArray, -15));
-		System.out.println(search.iterativeBinarySearch(intArray, 35));
+		intTree.insert(25);
+		intTree.insert(20);
+		intTree.insert(15);
+		intTree.insert(27);
+		intTree.insert(30);
+		intTree.insert(29);
+		intTree.insert(26);
+		intTree.insert(22);
+		intTree.insert(32);
 		
+		intTree.traverseInOrder();
 		
 	}			
 }
