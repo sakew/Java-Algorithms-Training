cc13aa6c6ab83658b42e147a899681710edf755f
sakew <dorinsecu@gmail.com>
2020-12-03 18:12:20 +0200

    challenge to remove duplicates from a hashtable using the jdk methods

--- Main.java
+++ Main.java
@@ -1,13 +1,46 @@
-import listsExercices.HashtableChallenge;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ListIterator;
+import listsExercices.Employee;
 
 public class Main {
 
 	public static void main(String[] args) {
 			
 		
-		HashtableChallenge challenge = new HashtableChallenge();
+		LinkedList<Employee> employees = new LinkedList<>();
+		employees.add(new Employee("Jane", "Jones", 123));
+		employees.add(new Employee("John", "Doe", 5678));
+		employees.add(new Employee("Mike", "Wilson", 45));
+		employees.add(new Employee("Mary", "Smith", 5555));
+		employees.add(new Employee("John", "Doe", 5678));
+		employees.add(new Employee("Bill", "End", 3948));
+		employees.add(new Employee("Jane", "Jones", 123));
 		
-		challenge.challenge();
+		
+		employees.forEach(e -> System.out.println(e));
+		
+		HashMap<Integer, Employee> employeeTable = new HashMap<>();
+		ListIterator<Employee> iterator = employees.listIterator();
+		List<Employee> remove = new ArrayList<>();
+		
+		while (iterator.hasNext()) {
+			Employee employee = iterator.next();
+			if (employeeTable.containsKey(employee.getId())) {
+				remove.add(employee);
+			}
+			else {
+				employeeTable.put(employee.getId(), employee);
+			}
+		}
+		
+		for (Employee employee: remove) {
+			employees.remove(employee);
+		}
+		System.out.println("------------------------");
+		employees.forEach(e -> System.out.println(e));
 	}
 			
 }
