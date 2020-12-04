649979190008125282ade9c1e608af81793dd39d
sakew <dorinsecu@gmail.com>
2020-12-04 19:06:58 +0200

    implementing delete method on Binary Search Tree for cases in which the node we want to delete is a leaf or has only one child

--- Trees/Tree.java
+++ Trees/Tree.java
@@ -24,6 +24,49 @@ public class Tree {
 		return null;
 	}
 	
+	public void delete(int value) {
+		root = delete(root, value);
+	}
+	
+	private TreeNode delete(TreeNode subtreeRoot, int value) {
+		
+// 		there is no subtreeRoot, return the current value;
+		
+		if (subtreeRoot == null) {
+			return subtreeRoot;
+		}
+		
+//		if the value of the subtreeRoot is smaller than the current value, delete the left child value
+//		from the tree.
+		
+		if (value < subtreeRoot.getData()) {
+			subtreeRoot.setLeftChild(delete(subtreeRoot.getLeftChild(), value));
+		}
+
+//		if the value of the subtreeRoot is higher than the current value, delete the right child value
+//		from the tree.
+		
+		else if (value > subtreeRoot.getData()) {
+			subtreeRoot.setRightChild(delete(subtreeRoot.getRightChild(), value));
+		}
+		else {
+			
+//		Cases in which node to delete has 0 or 1 child / children;
+//		if the right child is null meaning that this is a leaf, we don't need to set anything,
+//		just leave it null.			
+		
+			if (subtreeRoot.getLeftChild() == null) {
+				return subtreeRoot.getRightChild();
+			}
+			else if (subtreeRoot.getRightChild() == null) {
+				return subtreeRoot.getLeftChild();
+			}
+		}
+//		return the subtreeRoot with the new values set after the operations are done.
+		
+		return subtreeRoot;
+	}
+	
 	public int min() {
 // if root is null, meaning this is an empty tree,
 //		return smallest value in an integer.		
